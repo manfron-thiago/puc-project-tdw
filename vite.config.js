f1437e8d9e8c336f39255058df9306f1a056eeb1
Thiago <thiago.mfrn@gmail.com>
2024-09-23 00:29:59 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/puc-project-tdw/',
-});
+  base: "/puc-project-tdw",
+})
